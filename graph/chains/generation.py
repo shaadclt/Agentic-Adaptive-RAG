28ebaf0df9c678b33d4de5fdb007279582861df0
Mohamed Shaad <shaadclt@gmail.com>
2026-09-06 11:10:27 +0530

    update generation.py

--- graph/chains/generation.py
+++ graph/chains/generation.py
@@ -1,6 +1,8 @@
-from langchain import hub
+from langchain_classic import hub
 from langchain_core.output_parsers import StrOutputParser
+
 from model import llm_model
+
 
 llm = llm_model
 
@@ -10,8 +12,13 @@ generation_chain = prompt | llm | StrOutputParser()
 
 
 """
-The generation chain is responsible for creating the actual response to the user's question. 
-We leverage a proven RAG prompt from LangChain Hub that has been optimized for retrieval-augmented generation tasks. 
-This prompt template knows how to effectively combine retrieved context with the user's question to generate coherent, informative responses.
-The chain uses StrOutputParser ensures we get clean string output that can be easily processed by subsequent components.    
+The generation chain is responsible for creating the actual response to the user's question.
+We leverage a proven RAG prompt from LangChain Hub that has been optimized for
+retrieval-augmented generation tasks.
+
+This prompt template combines the retrieved context with the user's question
+to generate a coherent and informative response.
+
+The generation chain uses StrOutputParser to ensure that the model output is
+returned as a clean string that can be processed by subsequent graph nodes.
 """
