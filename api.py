7f0b2d30b0053aa1a2cc8bd7d6990acf0dff5a1d
Mohamed Shaad <shaadclt@gmail.com>
2026-09-13 16:00:36 +0530

    update api.py

--- api.py
+++ api.py
@@ -12,6 +12,7 @@ from ingestion import (
 )
 from observability import RunObserver
 from sources import extract_sources
+from fastapi.middleware.cors import CORSMiddleware
 
 
 api = FastAPI(
@@ -21,6 +22,17 @@ api = FastAPI(
         "powered by LangGraph."
     ),
     version="1.0.0",
+)
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
